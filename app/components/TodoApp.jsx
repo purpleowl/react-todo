666cd5b80862fc728d4738b62ce8a74e1a43b024
purpleowl <endo@en.local>
2016-11-27 11:33:00 +0900

    Add TodoApp component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+         todo-app.jsx
+      </div>
+    )
+  }
+  
+});
+
+module.exports = TodoApp;
